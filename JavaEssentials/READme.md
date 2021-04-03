577f157bbb1484dcfaa9600c72ab3cc1d5f3d6b8
Ravi-Papasani <76274650+Ravi-Papasani@users.noreply.github.com>
2021-04-03 02:09:24 -0500

    Java Variables and Types

---
+++ JavaEssentials/READme.md
@@ -0,0 +1,7 @@
+**Java Essentials**
+
+- Language Variables_And_Types_Syntax
+* Operators and Control Flows
+- OOP Concepts
+* Classes, Interfaces, Packages, Inheritance
+- Exception Handling
---
+++ JavaEssentials/src/main/java/Variables_And_Types_Syntax/_7_Operators.java
@@ -0,0 +1,55 @@
+package Variables_And_Types_Syntax;
+
+public class _7_Operators {
+    public static void main(String[] args) {
+        //Arithmetic operators
+        /*
+        Additive + -
+        Multiplicative * / %
+        * */
+
+        //Assignment operators
+        /*
+        Equals =
+        += -= *= /= %= &= ^= |= <<= >>= >>>=
+
+        int a += 5;
+        int a = a + 5;
+        * */
+
+        //Postfix and Prefix operators
+        /*
+        expr++  expr--
+        count++; (++count->prefix increment)
+        count = count + 1;
+
+        ++expr --expr
+        --count
+        count = count - 1;
+        * */
+
+        //Equality operators(returns always true or false)
+        /*
+        == !=
+
+        boolean isCentury = val == 100;
+        boolean isDoubleCentury = value != 200;
+        * */
+
+        //Relational operators
+        /*
+        < > <= >=
+        * */
+
+        //Logical operators
+        /*
+        && ||
+        * */
+
+        //Ternary operator
+        /*
+        ? :
+        boolean bagContent = isRaining ? umbrella : hat;
+        * */
+    }
+}
